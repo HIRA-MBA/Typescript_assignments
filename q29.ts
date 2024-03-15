84334ad948e8d3ddad7708b5e4170987ff9d4de3
HIRA-MBA <159155776+HIRA-MBA@users.noreply.github.com>
2024-03-15 20:24:33 +0500

    q29

---
+++ q29.ts
@@ -0,0 +1,12 @@
+//  task 29
+let fruits : string[]= ["apple", "banana","grapes","pineapple","mango"];
+if ( fruits.includes("banana"))
+{console.log("Banana is my favourite fruit")}
+if ( fruits.includes("apple"))
+{console.log("Apple is my favourite fruit")}
+if ( fruits.includes("grapes"))
+{console.log("Grapes is my favourite fruit")}
+if ( fruits.includes("pineapple"))
+{console.log("Pineapple is my favourite fruit")}
+if ( fruits.includes("mango"))
+{console.log("Mango is my favourite fruit")}
