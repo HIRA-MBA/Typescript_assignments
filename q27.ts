d9e918f29f8abbbee650ab8b5ca183b0044f6ded
HIRA-MBA <159155776+HIRA-MBA@users.noreply.github.com>
2024-03-15 20:22:31 +0500

    it includes q27 of assignment

---
+++ q27.ts
@@ -0,0 +1,29 @@
+let alien_Col = "green";
+// task # 27 
+console.log(" \n Task number 27 \n");
+
+if (alien_Col == "green")
+{ console.log("You have earned 5 points");}
+else if (alien_Col == "red"){ console.log(" You have earned 10 points");}
+else if (alien_Col == "yellow")
+{console.log ("you have earned 15 points");}
+
+alien_Col = "yellow";
+
+if (alien_Col == "green")
+{ console.log("You have earned 5 points");}
+else if (alien_Col == "red"){ console.log(" You have earned 10 points");}
+else if (alien_Col == "yellow")
+{console.log ("you have earned 15 points");}
+
+alien_Col = "red";
+
+if (alien_Col == "green")
+{ console.log("You have earned 5 points");}
+else if (alien_Col == "red"){ console.log(" You have earned 10 points");}
+else if (alien_Col == "yellow")
+{console.log ("you have earned 15 points");}
+
+
+
+ 
