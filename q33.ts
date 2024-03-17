7ddd791d575316b6e24c0085d53de8a62cf97025
HIRA-MBA <159155776+HIRA-MBA@users.noreply.github.com>
2024-03-17 21:26:18 +0500

    q33 for loop array

---
+++ q33.ts
@@ -0,0 +1,12 @@
+//  task number 33
+let ordinal_nos : number[] = [1,2,3,4,5,6,7,8,9]
+
+ for (var ord of ordinal_nos)
+ if (ord == 1){console.log(`${ord}st`)}
+ else if (ord == 2){console.log(`${ord}nd`)}
+ else if (ord == 3 ){console.log(`${ord}rd`)}
+ else {console.log(`${ord}th`)}
+
+
+    
+ 
