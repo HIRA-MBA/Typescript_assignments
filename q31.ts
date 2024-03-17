d12e74326a4d9a228b8f22091fd719d80b2522cb
HIRA-MBA <159155776+HIRA-MBA@users.noreply.github.com>
2024-03-17 21:29:20 +0500

    use splice to empty the array

---
+++ q31.ts
@@ -0,0 +1,13 @@
+//  task number 3
+
+let users: string[] = ["Eric", "Robbert","Sara","Marry", "admin"];
+
+for (var user of users)
+if (users.length==0){ console.log( " we need to find some users ")} 
+else {console.log(" Hello",user.toUpperCase())}
+users.splice(0)
+if (users.length==0){ console.log( " we need to find some users ")}
+
+
+
+
