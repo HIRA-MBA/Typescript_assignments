beb4157686383b2163611dbfb521c37ab6ff80ed
HIRA-MBA <159155776+HIRA-MBA@users.noreply.github.com>
2024-03-17 21:26:53 +0500

    q34 for loop for array

---
+++ q34.ts
@@ -0,0 +1,8 @@
+//  task 34
+var pizzas : string[] =["fagita","afgan feast","tikka","Bar B Q"]
+for (var pizza of pizzas)
+console.log(pizza)
+// modificatio in program
+for (pizza of pizzas){console.log(` I like ${pizza} pizza`)}
+console.log("i like pizza very much. \n Specially ",pizzas[0],
+"and " , pizzas[3], "\n Because of different flavour I really love pizza")
